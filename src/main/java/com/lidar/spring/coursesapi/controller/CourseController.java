e8dc8e440796743b6e61e74b711fa427b4c9ba16
Lidar Koma <lidarkoma@gmail.com>
2022-08-11 21:46:08 -0500

    first commit

--- src/main/java/com/lidar/spring/coursesapi/controller/CourseController.java
+++ src/main/java/com/lidar/spring/coursesapi/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.lidar.spring.coursesapi.model.Course;
 import com.lidar.spring.coursesapi.repository.CourseRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class CourseController {
 	
